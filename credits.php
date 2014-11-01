61c2499c103c8e6a33080da55417065c9381dc65
Tenzin Kunkyab <tkunkyab@tenkunkyab.local>
2014-11-01 22:06:21 +0530

    Updated Main Directory Content

--- credits.php
+++
@@ -1,8 +0,0 @@
-# Author : Tenzin Kunkyab ten.kunkyab@gmail.com
-<?php
-
-	echo "Project Title : Real Estate Project<br/>";
-	echo "Client        : xxxxxHiddenxxxxx <br/>";
-	echo "Developed By  : Tenzin Kunkyab <br/>";
-
-?>	
