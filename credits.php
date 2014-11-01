6a87d5db953bdd4b2c214770461f97e0fae91939
Tenzin Kunkyab <tkunkyab@tenkunkyab.local>
2014-11-01 21:55:28 +0530

    Added Credits and Test-Connection Mods

---
+++ credits.php
@@ -0,0 +1,8 @@
+# Author : Tenzin Kunkyab ten.kunkyab@gmail.com
+<?php
+
+	echo "Project Title : Real Estate Project<br/>";
+	echo "Client        : Anand Rathi <br/>";
+	echo "Developed By  : Tenzin Kunkyab <br/>";
+
+?>	
---
+++ test-connection.php
@@ -0,0 +1,7 @@
+# Author: Tenzin Kunkyab ten.kunkyab@gmail.com
+#
+# Primarily for Test Database Connectivity PHP
+# For now phpinfo();
+<?php
+	echo phpinfo();
+?>
