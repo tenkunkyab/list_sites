ae3cb0eec79e9c18798e255afbb118c050b147c9
Tenzin Kunkyab <ten.kunkyab@gmail.com>
2014-11-25 00:07:58 +0530

    Basic CodeIgniter MVC Framework First Commit

--- source_files/public/test-connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	# Author: Tenzin Kunkyab ten.kunkyab@gmail.com
-	#
-	# Primarily for Test Database Connectivity PHP
-	# For now phpinfo();
-	include_once('../sys/core/init.inc.php');
-	$inst = new Test_Module();
-		
-?>
--- source_files/sys/test-connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-	# Author: Tenzin Kunkyab ten.kunkyab@gmail.com 	
-	# 	
-	# Primarily for Test Database Connectivity PHP 	
-	# For now phpinfo(); 	 	
-	echo phpinfo(); 	
-
-?>
